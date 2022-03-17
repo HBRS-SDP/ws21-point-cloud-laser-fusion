b7d54058080dd884d7180de6ae3dfede2d8df0bf
SurajL <netrayvaine@gmail.com>
2022-03-17 18:04:15 +0100

    Implemented pointcloud and laserscan subscribers to run in parallel , filter used: Voxel grid only

---
+++ pointcloud_laserscan_fusion_voxelgrid_multithread/src/Fusion.h
@@ -0,0 +1,48 @@
+//ROS specific includes
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+#include <sensor_msgs/LaserScan.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_sensor_msgs/tf2_sensor_msgs.h>
+#include <tf2_ros/transform_listener.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
+//PCL Specific includes
+#include<pcl_conversions/pcl_conversions.h>
+#include<pcl/point_cloud.h>
+#include<pcl/point_types.h>
+#include<pcl/filters/voxel_grid.h>
+
+//CPP specific includes
+#include <string.h>
+#include <sstream>
+#include <thread>
+
+
+class Fusion
+{
+  public:
+
+  Fusion(ros::NodeHandle& nh, ros::NodeHandle& nh2);
+  void pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg);
+  void lasermessageCallback(const sensor_msgs::LaserScanConstPtr& laser_msg);
+  
+  private:
+
+  ros::NodeHandle nh_;
+  ros::NodeHandle nh2_;
+  boost::shared_ptr<tf2_ros::Buffer> tf2_;
+  boost::shared_ptr<tf2_ros::TransformListener> tf2Listener_;
+  int numRange_=-1;
+  ros::Subscriber laserSub_;
+  ros::Subscriber pcSub_;
+  ros::Publisher projectedLaserPub_;
+  ros::Publisher pointCloudPub_;
+  sensor_msgs::LaserScan projectedLaser_;
+  int numPoints_=0;
+  int pcCount_=0;
+};
+
---
+++ pointcloud_laserscan_fusion_voxelgrid_multithread/src/node_fusion.cpp
@@ -0,0 +1,30 @@
+
+#include"Fusion.h"
+
+int main(int argc, char **argv)
+{
+
+  
+  ros::init(argc, argv, "node_fusion");
+  ros::NodeHandle nh;
+  ros::NodeHandle nh2;
+  
+  // Dedicated spinner for PCL
+  ros::CallbackQueue callback_queue_pcl;
+  nh2.setCallbackQueue(&callback_queue_pcl);
+  Fusion fusion(nh, nh2);
+  ROS_INFO("initialized object");
+
+
+  std::thread spinner_thread_pcl([&callback_queue_pcl]() {
+    ros::SingleThreadedSpinner spinner_pcl;
+    spinner_pcl.spin(&callback_queue_pcl);
+  });
+  
+
+  ros::spin();
+
+  return 0;
+}
+
+
